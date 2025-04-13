01ffa706ac8c2c7496ee5b080f1526d36338c8e0
Henri Leinonen <hemalein@gmail.com>
2025-04-13 22:15:27 +0300

    Small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './theme';
 import Navbar from './components/Navbar';
